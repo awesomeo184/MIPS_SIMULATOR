0183bcc79578d02c39c62059e929e2852776c502
p-seungri <38969685+p-seungri@users.noreply.github.com>
2020-10-30 17:11:43 +0900

    Update main.c

--- main.c
+++ main.c
@@ -13,7 +13,16 @@ void setPC(unsigned int val) {
 }
 
 void find_register(unsigned int rn) {
+    unsigned char* register_table[] = { "$zero", "$at", "$v0", "$v1", "$a0", "$a1", "$a2", "$a3", "$t0", "$t1",
+    "$t2", "$t3", "$t4", "$t5", "$t6", "$t7", "$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7",
+    "$t8", "$t9", "$k0", "$k1", "$gp", "$sp", "$fp", "$ra" };
 
+    if (rn >= 0 && rn <= 31) {
+        printf("%s", register_table[rn]);
+    }
+    else {
+        printf("잘못된 레지스터 값이 입력되었습니다.\n");
+    }  
 }
 
 int ALU(unsigned int funct, int a, int b) {
