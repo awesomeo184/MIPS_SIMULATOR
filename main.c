6f5941c77cfd318ee539df88bdd33de69b970eaf
Jin-R <54504883+Jin-R@users.noreply.github.com>
2020-11-12 19:14:56 +0900

    Update function MEM

--- main.c
+++ main.c
@@ -40,7 +40,66 @@ int REG(int A, int v, int nRW) {
 
 //메모리 접근 함수
 int MEM(unsigned int A, int V, int nRW, int S) {
+    unsigned int sel, offset;
+	unsigned char* pM;
 
+	if (S < 0 || S > 2) {
+		printf("Wrong Memory Size Input\n Only can access with byte(0), halfword(1), or word(2)\n");
+		return 0;
+	}
+
+	sel = A >> 20;
+	offset = A & 0xFFFFF;
+
+	if (sel == 0x004)
+		pM = progMEM;         // program memory
+
+	else if (sel == 0x100)
+		pM = dataMEM;  // data memory
+
+	else if (sel == 0x7FF)
+		pM = stackMEM;  // stack
+
+	else {
+		printf("No memory\n");
+		exit(1);
+	}
+
+	if (S == 0) {  // byte
+		if (nRW == 0) { // read
+			return pM[offset];
+		}
+		else if (nRW == 1) { // write
+			pM[offset] = V;
+		}
+	}
+
+	else if (S == 1) { // half word
+		if (nRW == 0) { // read
+			return (pM[offset] | (pM[offset + 1] << 8));
+		}
+		else if (nRW == 1) { // write
+			pM[offset] = V;
+			pM[offset + 1] = V >> 8;
+		}
+	}
+
+	else if (S == 2) { // word
+		if (nRW == 0) { // read
+			return (pM[offset] | (pM[offset + 1] << 8) | (pM[offset + 2] << 16) | (pM[offset + 3] << 24));
+		}
+		else if (nRW == 1) { // write
+			pM[offset] = V;
+			pM[offset + 1] = V >> 8;
+			pM[offset + 2] = V >> 16;
+			pM[offset + 3] = V >> 24;
+		}
+	}
+
+	else {
+		printf("Wrong instrustions\n Only can Read(0), or Write(1)\n");
+		return 0;
+	}
 }
 
 // 명령어 처리
